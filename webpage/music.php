5981f3335b5513044ecdc5628763286926719c0d
SSKhodja <suxrobkhodja@gmail.com>
2018-03-11 21:53:26 +0500

    added shuffle

--- webpage/music.php
+++ webpage/music.php
@@ -24,45 +24,87 @@
 			<ul id="musiclist">
 			<?php
 				include("function.php");
-				foreach(glob("songs/*.mp3") as $file) 
+				$list = glob("songs/*.mp3");
+				if(isset($_REQUEST["shuffle"])){
+					if($_REQUEST["shuffle"]=="on"){
+						shuffle($list);	
+					}
+				}
+				// if(isset($_REQUEST["bysize"])){
+				// 	if($_REQUEST["bysize"]=="on"){
+				// 		$list_size;
+				// 		$list_sorted;
+				// 		for ($i=0; $i < count($list); $i++) {
+				// 			$list_sorted[$i] = $list[$i]; 
+				// 			$list_size[$i]=filesize($list[$i]);
+				// 		}
+				// 		for ($i=0; $i < count($list); $i++) {
+				// 			for ($j=0; $j < count($list); $j++) { 
+				// 				if ($list_size[$i]== filesize($list_sorted[$j]) && $list_sorted[$j]!= "E") {
+				// 					$list[$i]=$list_sorted[$j];
+				// 					$list_sorted[$j]="E";
+				// 				}
+				// 			}
+				// 		}	
+				// 	}
+				// }
+				foreach($list as $file) 
 				{	
 					if(isset($_REQUEST["playlist"])){
 						$tracks=file("songs/".$_REQUEST["playlist"]);
+						$actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+						if(isset($_REQUEST["shuffle"])){
+							if($_REQUEST["shuffle"]=="on"){
+								shuffle($tracks);	
+							}
+						}
 						foreach ($tracks as $value) {
 							
-							// $filepath="songs/".$value;	
-							// print($filepath);	
-			?>
+							$filepath="songs/".trim($value);
+							// print($filepath);
+								
+			?>				
 							<li class="mp3item">
-							<a href="songs/<?= $value?>"><?= $value /*."  " . formatSizeUnits(filesize($filepath))*/ ?></a>
+							<a href="songs/<?= $value?>" download><?= $value ."  " . formatSizeUnits(filesize($filepath)) ?></a>
 					    	</li>
 			<?php
 						}
+			?>
+						<li class="back-label">
+							<a href="music.php">Back</a>
+						</li>
+			<?php			
 						break; 
 					}
 					else{
 						// print($file);
 			?>	
 						<li class="mp3item">
-						<a href="songs/<?= basename($file)?>"><?= basename($file) ."  " . formatSizeUnits(filesize($file)) ?></a>
+						<a href="songs/<?= basename($file)?>" download><?= basename($file) ."  " . formatSizeUnits(filesize($file)) ?></a>
 						</li>
 			<?php 
 					} 
 				}
-
 			?>
+
 
 			<?php
-				foreach(glob("songs/*.txt") as $file) 
-				{
+				if(!isset($_REQUEST["playlist"])){
+					foreach(glob("songs/*.txt") as $file) 
+					{			
 			?>
-					<li class="playlistitem">
-					<a href="music.php?playlist=<?= basename($file)?>"><?= basename($file) ."  " . formatSizeUnits(filesize($file)) ?></a>
-			    	</li>
-			<?php		
+						<li class="playlistitem">
+						<a href="music.php?playlist=<?= basename($file)?>"><?= basename($file) ."  " . formatSizeUnits(filesize($file)) ?></a>
+				    	</li>
+			<?php	
+					}
+			?>
+					<li><a href="music.php?shuffle=on">SHUFFLE</a></li>
+					<li><a href="music.php?bysize=on">SORT BY SIZE</a></li>
+			<?php
 				}
-			?>
-
+			?>	
+						
 			</ul>
 
 
